f8384d277a2cda56be577540539daced76f5912f
Jaxson Hood <jaxson.hood@hotmail.com>
2021-12-22 23:52:48 -0500

    update to coming soon landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 
     // Load data from api endpoint on server
 	export const load = async ({ fetch }) => {
-        
+
         const posts = await db.getPosts()
         const message = posts[0].content
 
@@ -21,11 +21,16 @@
 </script>
 
 <svelte:head>
-	<title>SvelteKit + Tailwind + Vercel + Supabase</title>
-    <meta name="description" content="SvelteKit setup project with Tailwind + Supabase hosted on Vercel...">
+	<title>u-n-me</title>
+    <meta name="description" content="A new way to find people that are interesting!">
 </svelte:head>
 
-<main class="bg-black text-gray-200 text-xl w-full h-screen grid content-center justify-center text-center">
-    <div class="font-bold text-5xl">Hello From SvelteKit + Tailwind + Vercel + Supabase</div>
-    <div class="p-5 font-bold text-gray-400 font-mono">{ message }</div>
+<main class="bg-black text-gray-200 text-3xl w-full h-screen grid content-center justify-center text-center">
+    <div class="-mt-10 mx-auto">
+        <img class="w-72" src="/logo.png" alt="heart rings" />
+    </div>
+    <div class="font-bold text-8xl text-gray-100">u-<span class="text-gray-400">n-</span><span class=" text-gray-300">me</span></div>
+    <div class="p-5 font-semibold text-gray-600 font-mono">Coming Soon!</div>
+    <!-- <div class="font-bold text-5xl">Hello From SvelteKit + Tailwind + Vercel + Supabase</div> -->
+    <div class="p-5 font-thin text-xs text-gray-900">{ message }</div>
 </main>
